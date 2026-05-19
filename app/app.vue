79215236a6f40ceb0930ce39ffd2dded482575f8
Larra Su <larramsu@gmail.com>
2026-05-19 09:02:24 -0700

    feat: add useHead and useSeoMeta

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#09090b' : 'white')
+
 useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
   titleTemplate: title => title ? `${title} | My Nuxt Starter` : 'My Nuxt Starter',
+})
+
+const title = 'Nuxt Starter'
+const description = 'A simple Nuxt starter utilizing Nuxt\'s ecosystem.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
 })
 </script>
 
 <template>
-  <NuxtRouteAnnouncer />
   <UApp>
     <NuxtLayout>
       <NuxtPage />
